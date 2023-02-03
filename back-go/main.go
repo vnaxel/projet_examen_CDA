fce3d39d87f3c4d8f409f5faf4a666f7bb45f9da
ayvenou <axel.yvenou@docaposte.fr>
2023-02-03 13:54:56 +0100

    listen to recipient insert into mongo with go backend

--- back-go/main.go
+++ back-go/main.go
@@ -1,5 +1,47 @@
 package main
 
-import ()
+import (
+	"context"
+	"fmt"
+	"log"
 
-func main()	{}
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	collection := client.Database("sendings").Collection("recipients")
+
+	stream, err := collection.Watch(context.TODO(), mongo.Pipeline{
+		bson.D{{Key: "$match", Value: bson.D{{Key: "operationType", Value: "insert"}}}},
+	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for stream.Next(context.TODO()) {
+		var changeEvent map[string]interface{}
+		err := stream.Decode(&changeEvent)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(changeEvent["fullDocument"].(map[string]interface{})["deliveryStatuses"].(map[string]interface{}))
+	}
+
+	if err := stream.Err(); err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+func produceAndDeliver(event map[string]interface{}) {
+	
+}
