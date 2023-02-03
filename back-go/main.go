cd175cebd54cd75bf45299893a423d14ea8c6c2b
ayvenou <axel.yvenou@docaposte.fr>
2023-02-03 17:25:27 +0100

    refactored go script

--- back-go/main.go
+++ back-go/main.go
@@ -17,6 +17,37 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+type recipient struct {
+
+	Id 					primitive.ObjectID 	`bson:"_id" json:"_id"`
+	SendingId 			string 				`bson:"sendingId" json:"sendingId"`
+	SenderName			string				`bson:"senderName" json:"senderName"`
+	Address				string				`bson:"address" json:"address"`
+	FirstName			string				`bson:"firstName" json:"firstName"`
+	LastName			string				`bson:"lastName" json:"lastName"`
+	DeliveryStatuses 	deliveryStatuses	`bson:"deliveryStatuses" json:"deliveryStatuses"`
+	Version				int					`bson:"__v" json:"__v"`
+}
+
+type deliveryStatuses struct {
+
+	LastStatus 			status 				`bson:"lastStatus" json:"lastStatus"`
+	StatusesHistory 	[]status 			`bson:"statusesHistory" json:"statusesHistory"`
+}
+
+type status struct {
+
+	Status 				string 				`bson:"status" json:"status"`
+	Date 				time.Time			`bson:"date" json:"date"`
+}
+
+type recipientEvent struct {
+
+	OperationType 		string 				`bson:"operationType"`
+	ClusterTime 		primitive.Timestamp `bson:"clusterTime"`
+	FullDocument      	recipient           `bson:"fullDocument"`
+}
+
 var p, _ = producer.NewProducer([]string{"localhost:9092"})
 
 func main() {
@@ -37,12 +68,13 @@ func main() {
 	}
 
 	for stream.Next(context.TODO()) {
-		var changeEvent map[string]interface{}
+		var changeEvent recipientEvent
 		err := stream.Decode(&changeEvent)
 		if err != nil {
 			log.Fatal(err)
 		}
-		go produceAndDeliver(changeEvent)
+
+		go produceAndDeliver(changeEvent.FullDocument)
 	}
 
 	if err := stream.Err(); err != nil {
@@ -51,25 +83,19 @@ func main() {
 	}
 }
 
-func produceAndDeliver(event map[string]interface{}) {
+func produceAndDeliver(recipient recipient) {
 
 	// STAGE 1 PRODUCED EVENT (10-20 seconds)
 	duration := time.Duration(rand.Intn(20-10+1) + 10) * time.Second
 	time.Sleep(duration)
 
-	statusesHistory := event["fullDocument"].(map[string]interface{})["deliveryStatuses"].(map[string]interface{})["statusesHistory"]
-	lastStatus := event["fullDocument"].(map[string]interface{})["deliveryStatuses"].(map[string]interface{})["lastStatus"]
+	recipient.DeliveryStatuses.StatusesHistory = append(recipient.DeliveryStatuses.StatusesHistory, recipient.DeliveryStatuses.LastStatus)
+	recipient.DeliveryStatuses.LastStatus = status{Date: time.Now().Round(3 * time.Millisecond).UTC(), Status: "PRODUCED"}
 
-	statusesHistory = append(statusesHistory.(primitive.A), lastStatus)
-	lastStatus = map[string]interface{}{"date": time.Now().Round(3 * time.Millisecond), "status": "PRODUCED"}
+	fmt.Println(recipient)
 
-	event["fullDocument"].(map[string]interface{})["deliveryStatuses"].(map[string]interface{})["statusesHistory"] = statusesHistory
-	event["fullDocument"].(map[string]interface{})["deliveryStatuses"].(map[string]interface{})["lastStatus"] = lastStatus
-
-	fmt.Println(event["fullDocument"].(map[string]interface{})["deliveryStatuses"].(map[string]interface{}))
-
-	eventMarshal, _ := json.Marshal(event["fullDocument"].(map[string]interface{})["deliveryStatuses"].(map[string]interface{}))
-	idMarshal, _ := json.Marshal(event["fullDocument"].(map[string]interface{})["_id"].(primitive.ObjectID))
+	eventMarshal, _ := json.Marshal(recipient.DeliveryStatuses)
+	idMarshal, _ := json.Marshal(recipient.Id)
 
 	p.Input <- &sarama.ProducerMessage{
 		Topic: "statuses_topic",
@@ -83,23 +109,20 @@ func produceAndDeliver(event map[string]interface{}) {
 	//get a random string within those two "DELIVERED" "INVALID_ADDRESS" with 80% probability for DELIVERED and 20% probability for INVALID_ADDRESS
 	rand.Seed(time.Now().UnixNano())
 	random := rand.Intn(100)
-	var status string
+	var randomStatus string
 	if random < 80 {
-		status = "DELIVERED"
+		randomStatus = "DELIVERED"
 	} else {
-		status = "INVALID_ADDRESS"
+		randomStatus = "INVALID_ADDRESS"
 	}
 
-	statusesHistory = append(statusesHistory.(primitive.A), lastStatus)
-	lastStatus = map[string]interface{}{"date": time.Now().Round(3 * time.Millisecond), "status": status}
+	recipient.DeliveryStatuses.StatusesHistory = append(recipient.DeliveryStatuses.StatusesHistory, recipient.DeliveryStatuses.LastStatus)
+	recipient.DeliveryStatuses.LastStatus = status{Date: time.Now().Round(3 * time.Millisecond).UTC(), Status: randomStatus}
 
-	event["fullDocument"].(map[string]interface{})["deliveryStatuses"].(map[string]interface{})["statusesHistory"] = statusesHistory
-	event["fullDocument"].(map[string]interface{})["deliveryStatuses"].(map[string]interface{})["lastStatus"] = lastStatus
+	fmt.Println(recipient)
 
-	fmt.Println(event["fullDocument"].(map[string]interface{})["deliveryStatuses"].(map[string]interface{}))
-
-	eventMarshal2, _ := json.Marshal(event["fullDocument"].(map[string]interface{})["deliveryStatuses"].(map[string]interface{}))
-	idMarshal2, _ := json.Marshal(event["fullDocument"].(map[string]interface{})["_id"].(primitive.ObjectID))
+	eventMarshal2, _ := json.Marshal(recipient.DeliveryStatuses)
+	idMarshal2, _ := json.Marshal(recipient.Id)
 
 	p.Input <- &sarama.ProducerMessage{
 		Topic: "statuses_topic",
